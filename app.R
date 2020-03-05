b86779b568e23e3e8ce34dc5d542bad948c6e277
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-03-05 05:02:15 -0800

    server side linking

--- app.R
+++ app.R
@@ -1,11 +1,19 @@
 library(shiny)
+library(shinythemes)
 
 # Load the date once
 # Load the RDS file
 list_objs <- readRDS('data/data.rds')
 
 mds_df <- list_objs$mds_df
+rownames(mds_df) <- c(1:dim(mds_df)[1])
 # Plotting mds parameters
+# Conf modebar
+conf_ <- c("zoomIn2d", "zoomOut2d", "select2d", 
+          "toImage", "autoScale2d",
+          "toggleSpikelines", "hoverCompareCartesian", 
+          "hoverClosestCartesian") 
+# Axes
 ax_mds <- list(linecolor = toRGB('black'), automargin = T, 
                scaleratio =1, scaleanchor = "x",  dtick = 0.25, linewidth = 2,
                zerolinecolor = toRGB('grey'), zerolinewidth = 2, mirror = T, showline = T)
@@ -20,12 +28,13 @@ text_hover <- ~paste('<b>PDB id:</b>', mds_df$X,
                      '<br><b>Ligand:</b>', mds_df$Inhib)
 marker_size = 5
 # Colors
-pal <- c("#f64b3c", "#5b8c5a", "#0f4c75", "#feb72b", "#29c7ac")
+pal <- c("#f64b3c", "#5b8c5a", "#2a7886", "#feb72b", "#29c7ac")
 pal <- setNames(pal, c('active', 'inact_a', 'inact_b', 'inact_ope', 'dfg_out'))
 pal_violin <- c('#2a7886', '#f1935c', '#ed7575')
 
 
 ui <- fluidPage(
+    theme=shinytheme('journal'),
 
     # Application title
     titlePanel("CDK2: Consensus/ML Scorings"),
@@ -54,13 +63,14 @@ ui <- fluidPage(
             fluidRow(
                 column(6, plotlyOutput(outputId = 'mdsPlot')),
                 column(6, plotlyOutput(outputId = 'swarmPlot'))
-            )
+            ),
+            verbatimTextOutput("click")
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     #*** MDS Parameters ***
     mds_subspaces <- reactive({
         switch (input$mds_subspace,
@@ -75,7 +85,11 @@ server <- function(input, output) {
             'Ligand Efficiency Score' = 'dklef')
         dk_columns_ <- grep(score_, colnames(mds_df), ignore.case =T, value = T)
     })
-    
+   
+    clicked_point <- reactive({
+        d <- event_data("plotly_click")
+        d$key
+    }) 
     
     #####################
     #***** OUTPUTS *****#
@@ -84,30 +98,58 @@ server <- function(input, output) {
         fig_mds <- plot_ly(type = 'scatter', mode = 'markers', 
                            data = mds_df, colors = pal) %>% 
             add_trace(x = mds_df[[mds_[1]]], y = mds_df[[mds_[2]]],
+                      key = rownames(mds_df),
                       color = mds_df[['Labels_conf']], 
                       text = text_hover,
                       size = mds_df$Inhib_mass,
                       hovertemplate = paste('%{text}')) %>% 
-            layout(xaxis = xax_mds, yaxis = yax_mds, dragmode = 'pan')
+            layout(xaxis = xax_mds, yaxis = yax_mds, dragmode = 'pan',
+                   title = list(text = paste0('cMDS subspace (',
+                                              input$mds_subspace, ')'), x = 0.1)) %>%
+            config(modeBarButtonsToRemove = conf_, displaylogo = FALSE) %>%
+            event_register("plotly_click")
+        # Add selected point
+        key <- clicked_point()
+        if (!is.null(key)) {
+            sliced_df <- mds_df[key, ]
+            fig_mds <- add_trace(fig_mds, x = sliced_df[[mds_[1]]], 
+                                      y = sliced_df[[mds_[2]]],
+                                      size = 8,
+                                      marker = list(color = 'rgba(0, 0, 0, 0)',
+                                      line = list(color = 'rgba(0, 0, 0, 1)',
+                                      width = 2)), name = 'Selected')
+        }
         fig_mds
     })
     
+    #**** Violin plots ****
     output$swarmPlot <- renderPlotly({
         # DkScore and DkLEff
-        i = 0
         score_cols <- dk_score()
+        point <- clicked_point()
         fig_swarm <- plot_ly(type = 'violin', data = mds_df, 
-                             boxpoints = "all", jitter = 1,
-                             selectedpoints = c(1,2), text = text)
+                             points = "all", jitter = 1,
+                             selectedpoints = c(point), text = text)
+        i = 0
         for (score_ in score_cols) {
+            i = i + 1
             fig_swarm <- add_trace(fig_swarm, y = mds_df[[score_]], pointpos = 0, 
                       marker = list(size = marker_size), 
                       name = strsplit(score_, '_')[[1]][1],
-                      color = I(pal_violin[i + 1]))
+                      color = I(pal_violin[i]),
+                      box = list(visible = T), key = rownames(mds_df))
         }
-        fig_swarm <- layout(fig_swarm, xaxis = xax_sw, yaxis = yax_sw, dragmode = 'pan') %>% 
-        highlight(on = "plotly_hover", off = "plotly_doubleclick")
+        fig_swarm <- layout(fig_swarm, xaxis = xax_sw, yaxis = yax_sw, 
+                            dragmode = 'pan', 
+                            title = list(text = paste0('AUC values (Vinardo ',
+                                          input$dk_score, ')'), x = 0.1)) %>% 
+        config(modeBarButtonsToRemove = conf_, displaylogo = FALSE)
         fig_swarm
+    })
+    
+    output$click <- renderPrint({
+        d <- event_data("plotly_click")
+        if (is.null(d)) "Click events appear here (double-click to clear)" else d
     })
 }
 
