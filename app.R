01090d48101e3a3a5c7310d93df89328fb25ecab
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-03-05 02:04:01 -0800

    Violinplot finished

--- app.R
+++ app.R
@@ -1,18 +1,29 @@
 library(shiny)
 
 # Load the date once
-# Load the mds data
-mds_data <- data.frame(t(read.csv('data/mds_subspaces_pis_pk.csv', header = FALSE)))
-subspace_names = c('pisani', 'pocket')
-n_dims = paste0('dim', c(1:2))
-# Name the columns dataframe
-colnames(mds_data) <- paste(rep(subspace_names, each = length(n_dims)), n_dims, sep = '.')
+# Load the RDS file
+list_objs <- readRDS('data/data.rds')
 
+mds_df <- list_objs$mds_df
 # Plotting mds parameters
-ax_mds <- list(linecolor = toRGB('black'), linewidth = 2, automargin = T, scaleratio =1, scaleanchor = "x",
+ax_mds <- list(linecolor = toRGB('black'), automargin = T, 
+               scaleratio =1, scaleanchor = "x",  dtick = 0.25, linewidth = 2,
                zerolinecolor = toRGB('grey'), zerolinewidth = 2, mirror = T, showline = T)
 yax_mds <- ax_mds; yax_mds[['title']] <- 'Second Dimension'
 xax_mds <- ax_mds; xax_mds[['title']] <- 'First Dimension'
+# Violin axis
+yax_sw <- ax_mds; yax_sw[['title']] <- 'ROC-AUC'; yax_sw[['range']] <- c(0.3, 1)
+xax_sw <- ax_mds; xax_sw[['title']] <- 'Databases'
+# Text hover
+text_hover <- ~paste('<b>PDB id:</b>', mds_df$X,
+                     '<br><b>Conf:</b>', mds_df$Labels_conf,
+                     '<br><b>Ligand:</b>', mds_df$Inhib)
+marker_size = 5
+# Colors
+pal <- c("#f64b3c", "#5b8c5a", "#0f4c75", "#feb72b", "#29c7ac")
+pal <- setNames(pal, c('active', 'inact_a', 'inact_b', 'inact_ope', 'dfg_out'))
+pal_violin <- c('#2a7886', '#f1935c', '#ed7575')
+
 
 ui <- fluidPage(
 
@@ -29,15 +40,20 @@ ui <- fluidPage(
                     inputId = 'mds_subspace',
                     label = 'cMDS subspace',
                     choices = c('Pisani Resiudes', 'Pocket Residues')
-                ))
-            )
+                ))),
+            fluidRow(
+                column(6, selectInput(
+                    inputId = 'dk_score',
+                    label = 'Docking score type:',
+                    choices = c('Docking Score', 'Ligand Efficiency Score')
+                )))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
             fluidRow(
-                column(6, plotlyOutput(outputId = 'mdsPlot'))
-                #column(6, plotOutput(outputId = 'distPlot_2'))
+                column(6, plotlyOutput(outputId = 'mdsPlot')),
+                column(6, plotlyOutput(outputId = 'swarmPlot'))
             )
         )
     )
@@ -46,22 +62,52 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     #*** MDS Parameters ***
-    mds_dataset <- reactive({
-        mds_ <- switch (input$mds_subspace,
-            'Pisani Resiudes' = mds_data[,1:2],
-            'Pocket Residues' = mds_data[,3:4]
+    mds_subspaces <- reactive({
+        switch (input$mds_subspace,
+            'Pisani Resiudes' = c('pisani.dim1', 'pisani.dim2'),
+            'Pocket Residues' = c('pocket.dim1', 'pocket.dim2')
         )
-        colnames(mds_) <- n_dims
-        mds_
     })
     
+    dk_score <- reactive({
+        score_ <- switch (input$dk_score,
+            'Docking Score' = 'dksc',
+            'Ligand Efficiency Score' = 'dklef')
+        dk_columns_ <- grep(score_, colnames(mds_df), ignore.case =T, value = T)
+    })
+    
+    
+    #####################
     #***** OUTPUTS *****#
     output$mdsPlot <- renderPlotly({
-        mds_ <- mds_dataset()
-        mds_plot <- plot_ly(data = mds_, x = mds_$dim1, y = mds_$dim2)
-        mds_plot <- mds_plot %>% layout(xaxis = xax_mds, yaxis = yax_mds, 
-                                        dragmode = 'pan')
-        mds_plot
+        mds_ <- mds_subspaces()
+        fig_mds <- plot_ly(type = 'scatter', mode = 'markers', 
+                           data = mds_df, colors = pal) %>% 
+            add_trace(x = mds_df[[mds_[1]]], y = mds_df[[mds_[2]]],
+                      color = mds_df[['Labels_conf']], 
+                      text = text_hover,
+                      size = mds_df$Inhib_mass,
+                      hovertemplate = paste('%{text}')) %>% 
+            layout(xaxis = xax_mds, yaxis = yax_mds, dragmode = 'pan')
+        fig_mds
+    })
+    
+    output$swarmPlot <- renderPlotly({
+        # DkScore and DkLEff
+        i = 0
+        score_cols <- dk_score()
+        fig_swarm <- plot_ly(type = 'violin', data = mds_df, 
+                             boxpoints = "all", jitter = 1,
+                             selectedpoints = c(1,2), text = text)
+        for (score_ in score_cols) {
+            fig_swarm <- add_trace(fig_swarm, y = mds_df[[score_]], pointpos = 0, 
+                      marker = list(size = marker_size), 
+                      name = strsplit(score_, '_')[[1]][1],
+                      color = I(pal_violin[i + 1]))
+        }
+        fig_swarm <- layout(fig_swarm, xaxis = xax_sw, yaxis = yax_sw, dragmode = 'pan') %>% 
+        highlight(on = "plotly_hover", off = "plotly_doubleclick")
+        fig_swarm
     })
 }
 
